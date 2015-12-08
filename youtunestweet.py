a7d51ca6922a12d91efb9aab280c492ed60bebcc
jntkym <ru6r1ckcu6e@gmail.com>
2015-12-08 12:33:18 +0900

    add returning error if list is empty

--- youtunestweet.py
+++ youtunestweet.py
@@ -5,7 +5,6 @@
 # recall: 0.83 (estimate)
 
 # fix: いちいちjsonにおとし込まなくてもリストのまま処理させるほうが自然
-# fix: リストが空のときはどうするか
 # todo: returnPlausibleVideoId()の処理がnot smart
 
 import sys
@@ -22,7 +21,7 @@ TWITTER_AS = 'wu31x3zWd3yUoj5hZefL9kLtRdGe64jl7nfQ2fw900oBc'
 # 曲名でYouTubeから動画リストを所得
 def searchSong(keyword):
   url = "https://www.googleapis.com/youtube/v3/search?key=%s&q=%s&part=id&maxResults=3" % (GOOGLE_API_KEY, keyword)
-  print "Getting video information from YouTube by querying %s ...\n" % url
+  print "Getting video information from YouTube by querying %s ..." % keyword, 
   r = requests.get(url)
   f = open('video_list.json', 'w')
   f.write(r.content)
@@ -34,29 +33,37 @@ def returnPlausibleVideoID():
   f = open('video_list.json', 'r')
   j = json.load(f)
   j = j.get('items')
-  print("\nGetting the plausible song url...\n")
-  return j[0]['id']['videoId']
+  if not j:
+    print "Error: Cannot find the song on YouTube",
+    return False
+  else:
+    print "Getting the plausible song url...",
+    return j[0]['id']['videoId']
 
 # 曲および動画情報をツイート
 def tweetSong(keyword, video_id):
-  url = "https://api.twitter.com/1.1/statuses/update.json"
-  tweet = "%s youtu.be/%s #nowplaying" % (keyword, video_id)
-  params = {"status": tweet}
-  twitter = OAuth1Session(TWITTER_CK, TWITTER_CS, TWITTER_AT, TWITTER_AS)
-  print("Tweeting the song information...")
-  req = twitter.post(url, params = params)
-  if req.status_code == 200:
-    print("OK")
-  else:
-    print("Error: %d" % req_status_code)
+  if video_id == False:
+    print "Error: Cannot tweet the song information", 
+  else: 
+    url = "https://api.twitter.com/1.1/statuses/update.json"
+    tweet = "%s youtu.be/%s #nowplaying" % (keyword, video_id)
+    params = {"status": tweet}
+    twitter = OAuth1Session(TWITTER_CK, TWITTER_CS, TWITTER_AT, TWITTER_AS)
+    print "Tweeting the song information...",
+    req = twitter.post(url, params = params)
+    if req.status_code == 200:
+      print "OK", 
+    else:
+      print "Error: %d" % req_status_code,
 
 def main():
   kw = sys.argv[1] + " - " + sys.argv[2]
   searchSong(kw)
   song_video_id = returnPlausibleVideoID()
+  print song_video_id,
   f = tweetSong(kw, song_video_id)
-  if f == FALSE:
-    print("Error")
+  #if f == FALSE:
+  #  print("Error")
 
 if __name__ == '__main__':
   main()
